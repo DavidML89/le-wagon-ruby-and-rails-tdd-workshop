fa07ee9a3eeb4149fb2a981dad96fed7c8b48aaf
David Morio <david.morio.lesavourey@gmail.com>
2022-06-20 17:40:52 +0200

    Excercise 2 done

---
+++ exercise_02/start/hello_tdd_world.rb
@@ -0,0 +1,17 @@
+class HelloTddWorld
+  def self.default
+    return "Hello, TDD World!"
+  end
+
+  def self.personalized(name)
+    return "Hello #{name}, TDD superstar!"
+  end
+
+  def self.random
+    [
+      "Red - Green - Refactor, baby!",
+      "TDD will change your life!",
+      "I came in red, I saw green, I refactored. - Julius Cesar"
+    ]
+  end
+end
--- exercise_02/start/hello_tdd_world_test.rb
+++ exercise_02/start/hello_tdd_world_test.rb
@@ -1,1 +1,22 @@
 require_relative "./check"
+require_relative "./hello_tdd_world"
+
+class HelloTddWorldTest < Check
+  def check_returns_default_tdd_greeting
+    is_equal?(HelloTddWorld.default, "Hello, TDD World!")
+  end
+
+  def check_returns_personalized_greeting
+    is_equal?(HelloTddWorld.personalized('Mary'), "Hello Mary, TDD superstar!")
+    is_equal?(HelloTddWorld.personalized('David'), "Hello David, TDD superstar!")
+  end
+
+  def check_returns_random_greeting
+    expected_phrases = [
+                         'Red - Green - Refactor, baby!',
+                         'TDD will change your life!',
+                         'I came in red, I saw green, I refactored. - Julius Cesar'
+                       ]
+    is_equal?(expected_phrases, HelloTddWorld.random)
+  end
+end
